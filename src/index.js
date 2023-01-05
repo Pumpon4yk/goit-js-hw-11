4825688a227a1e5cc690a39afeb1367d70fef9a7
Pumpon4yk <tt10392@gmail.com>
2023-01-05 21:05:48 +0100

    setap

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const URL ='https://pixabay.com/api/';
+const MY_KEY = '32634738-c4f3b04753ac88df1d3aaa7b6'
